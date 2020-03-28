7ea6887a7ecb599b25009458c5cfeb3c2a0fd638
siddharthshenoy <56694590+siddharthshenoy@users.noreply.github.com>
2020-03-28 16:23:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    
+    firebase.auth().signInWithPopup(provider)
+        
+            .then(result => {
+                const user = result.user;        
+            })  
+}
